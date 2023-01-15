019a67e5adfaf9c2396708083ad726c8f3dcece2
krutikabhadane77 <krutikabhadane588@gmail.com>
2023-01-15 19:34:33 +0530

    Added: UC2 Extend greetingcontroller to use services layer to get simple greeting message

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,5 +1,5 @@
 package com.example.greetingapp.controller;
-
+import com.example.greetingapp.service.IGreetingService;
 import com.example.greetingapp.model.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +13,20 @@ public class GreetingController {
 
     @Autowired
     private static final String template = "Hello, %s";
+    private IGreetingService greetingService;
     private final AtomicLong counter= new AtomicLong();
+    public GreetingController(IGreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     @GetMapping("/")
     public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return greetingService.getGreetingMessage();
     }
 
 }
---
+++ src/main/java/com/example/greetingapp/service/IGreetingService.java
@@ -0,0 +1,5 @@
+package com.example.greetingapp.service;
+
+public interface IGreetingService {
+    String getGreetingMessage();
+}
